ed12b98d5aa779aaf01e2bb9e92f520d1868602f
patryk-polanski <pat.polanski@gmail.com>
2025-09-21 00:56:52 +0200

    initial commit, set up basic node server

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    // "Content-Type": "text/plain",
+    "Content-Type": "application/json",
+  });
+  //   res.end("Hello! Sir Isaac Newton is your friend!");
+  res.end(
+    JSON.stringify({
+      id: 1,
+      name: "Sir Isaac Newton",
+    })
+  );
+});
+
+// 127.0.0.1 => localhost
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
