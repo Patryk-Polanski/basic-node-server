3ab6597cff53923ef86ef971ec176e9a84de209d
patryk-polanski <pat.polanski@gmail.com>
2025-09-21 01:13:15 +0200

    add basic routing with different content types in responses

--- index.js
+++ index.js
@@ -2,18 +2,39 @@ const http = require("http");
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    // "Content-Type": "text/plain",
-    "Content-Type": "application/json",
-  });
-  //   res.end("Hello! Sir Isaac Newton is your friend!");
-  res.end(
-    JSON.stringify({
-      id: 1,
-      name: "Sir Isaac Newton",
-    })
-  );
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  if (req.url === "/friends") {
+    res.writeHead(200, {
+      // "Content-Type": "text/plain",
+      "Content-Type": "application/json",
+    });
+    //   res.end("Hello! Sir Isaac Newton is your friend!");
+    res.end(
+      JSON.stringify({
+        id: 1,
+        name: "Sir Isaac Newton",
+      })
+    );
+  } else if (req.url === "/messages") {
+    // equivalent to the res.writeHead above
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+
+    res.write("<html>");
+    res.write("<body>");
+    res.write("<ul>");
+    res.write("<li>Hello Isaac!</li>");
+    res.write("<li>What are your thoughts on astronomy?</li>");
+    res.write("</ul>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
 // 127.0.0.1 => localhost
